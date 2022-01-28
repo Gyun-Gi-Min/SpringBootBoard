ed632193e464c231e67d73bf924f7366687a1bf2
GYUN-GI MIN <mingg95@gmail.com>
2022-01-28 14:41:41 +0900

    회원가입 완료

---
+++ src/main/java/com/koreait/springbootboard/board/model/BoardCategoryEntity.java
@@ -0,0 +1,13 @@
+package com.koreait.springbootboard.board.model;
+
+import lombok.Data;
+
+@Data
+public class BoardCategoryEntity {
+
+    private int icategory;
+    private String nm;
+    private int orderby;
+
+
+}
